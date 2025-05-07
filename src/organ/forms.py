efdc04b7c0905b77cb14256828d51a962d8bb8fe
chrtxachok <egormalanin425@gmail.com>
2025-05-07 23:21:17 +0700

    улучшение регистрации
    добавил оповещения об ошибках и глазок

--- src/organ/forms.py
+++ src/organ/forms.py
@@ -17,12 +17,11 @@ class TaskStatusForm(forms.ModelForm):
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Tasks
-        fields = ["title", "descriptionn", "deadline"]
+        fields = ["title", "descriptionn"]
 
         labels = {
             "title": "Название",
             "descriptionn": "Описание",
-            "deadline": "Дедлайн",
         }
 
         widgets = {
@@ -37,13 +36,6 @@ class TaskForm(forms.ModelForm):
                 attrs={
                     "placeholder": "Опишите задачу",
                 }
-            ),
-            "deadline": forms.DateInput(
-                attrs={
-                    'type': 'date',
-                    'class': 'form-control'
-                },
-                format='%Y-%m-%d'
             ),
         }
 
@@ -67,22 +59,32 @@ class RegisterForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        
+        # Удаляем стандартные подсказки
         for field_name in self.fields:
             self.fields[field_name].help_text = None
 
+        # Настраиваем атрибуты полей
         self.fields["username"].widget.attrs.update(
             {"id": "id_username", "placeholder": " "}
         )
         self.fields["email"].widget.attrs.update(
             {"id": "id_email", "placeholder": " "}
-        )  # noqa: E501
+        )
         self.fields["password1"].widget.attrs.update(
-            {"id": "id_password1", "placeholder": " "}
+            {"id": "id_password1", "placeholder": " ", "class": "password-field"}
         )
         self.fields["password2"].widget.attrs.update(
-            {"id": "id_password2", "placeholder": " "}
+            {"id": "id_password2", "placeholder": " ", "class": "password-field"}
         )
+
+        # Добавляем кастомные сообщения об ошибках
+        self.fields['password1'].error_messages = {
+            'password_too_short': 'Пароль должен содержать минимум 6 символов',
+            'password_too_common': 'Этот пароль слишком простой',
+            'password_entirely_numeric': 'Пароль не может состоять только из цифр',
+            'password_similar_to_username': 'Пароль слишком похож на имя пользователя',
+        }
 
 
 class CustomAuthenticationForm(AuthenticationForm):
@@ -95,6 +97,6 @@ class CustomAuthenticationForm(AuthenticationForm):
     password = forms.CharField(
         label="",
         widget=forms.PasswordInput(
-            attrs={"class": "form-control", "placeholder": ""}
+            attrs={"class": "form-control password-field", "placeholder": ""}
         ),  # noqa: E501
     )
--- src/organ/views.py
+++ src/organ/views.py
@@ -15,6 +15,7 @@ from .forms import (  # noqa: E501
     SubtasksStatusForm
 )
 from .models import Tasks, Subtasks
+
 from django.http import JsonResponse
 
 
